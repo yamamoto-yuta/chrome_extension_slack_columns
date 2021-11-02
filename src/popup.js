12eee434678eee43712a758ae60981e281d3847b
yamamoto-yuta <automatuX78@gmail.com>
2021-11-03 00:51:39 +0900

    🎨 フォーマット

--- src/popup.js
+++ src/popup.js
@@ -22,10 +22,10 @@ function addInput(url = '') {
 window.onload = function () {
     chrome.storage.sync.get(
         'channels',
-        function(channels) {
+        function (channels) {
             console.log(channels)
-            for(let i = 0; i < channels['channels'].length; i++) {
-                if(channels['channels'][i]){
+            for (let i = 0; i < channels['channels'].length; i++) {
+                if (channels['channels'][i]) {
                     addInput(channels['channels'][i])
                 }
             }
@@ -33,23 +33,20 @@ window.onload = function () {
     );
 
     let submitBtn = document.getElementById("submitLine");
-    submitBtn.onclick = function() {
+    submitBtn.onclick = function () {
         channels = []
-        for (let line of document.getElementsByClassName('line')){
-            if(line.value){
+        for (let line of document.getElementsByClassName('line')) {
+            if (line.value) {
                 channels.push(line.value)
             }
         }
         var formOptions = {
             channels: channels
         };
-        chrome.storage.sync.set(formOptions, function(){});
+        chrome.storage.sync.set(formOptions, function () { });
 
     }
 
     let addBtn = document.getElementById("addLine");
-    addBtn.onclick = function() {addInput()}
+    addBtn.onclick = function () { addInput() }
 }
-
-
-
