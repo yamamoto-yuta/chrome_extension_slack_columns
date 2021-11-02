986844b9dab47aa4dc21547f8cd0cdd9debfd32e
yamamoto-yuta <automatuX78@gmail.com>
2021-11-03 00:53:18 +0900

    💄 ポップアップを整えた #5

--- src/popup.js
+++ src/popup.js
@@ -1,19 +1,23 @@
 
 function addInput(url = '') {
     let div = document.createElement('div');
+    div.className = "flex-box my-1";
 
     let line = document.createElement('input');
     line.type = "text";
     line.value = url;
-    line.className = "line";
+    line.className = "form-control mx-1 line";
 
-    let delBtn = document.createElement('input');
-    delBtn.type = "button";
-    delBtn.value = "-";
-    delBtn.onclick = function() {div.remove();}
+    let delBtn = document.createElement('button');
+    delBtn.className = "btn btn-danger mx-1";
+    delBtn.onclick = function () { div.remove(); }
+
+    let delIcon = document.createElement('i');
+    delIcon.className = "bi bi-dash";
 
     div.appendChild(line);
     div.appendChild(delBtn);
+    delBtn.appendChild(delIcon);
 
     let lines = document.getElementById("lines");
     lines.appendChild(div);
