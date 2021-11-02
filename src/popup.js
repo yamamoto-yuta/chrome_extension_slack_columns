8d6ca1e8cd3ffdc3c7663f5ab046e4c69bc31c63
yamamoto-yuta <automatuX78@gmail.com>
2021-11-03 01:53:10 +0900

    💄 カラム幅を指定できるフォームを追加 #2

--- src/popup.js
+++ src/popup.js
@@ -3,10 +3,34 @@ function addInput(url = '') {
     let div = document.createElement('div');
     div.className = "flex-box my-1";
 
+    // Column width
+
+    let colWidth = document.createElement('select');
+    colWidth.className = "form-select";
+    colWidth.style.width = "auto";
+
+    let widthOptionList = [
+        { "text": "Narrow", "value": "300px" },
+        { "text": "Medium", "value": "500px" },
+        { "text": "Wide", "value": "700px" },
+    ]
+    for (let widthOption of widthOptionList) {
+        let colWidthOption = document.createElement('option');
+        colWidthOption.text = widthOption.text;
+        colWidthOption.value = widthOption.value;
+
+        colWidth.appendChild(colWidthOption);
+    }
+    colWidth.selectedIndex = 1;
+
+    // URL input
+
     let line = document.createElement('input');
     line.type = "text";
     line.value = url;
     line.className = "form-control mx-1 line";
+
+    // Delete button
 
     let delBtn = document.createElement('button');
     delBtn.className = "btn btn-danger mx-1";
@@ -15,9 +39,13 @@ function addInput(url = '') {
     let delIcon = document.createElement('i');
     delIcon.className = "bi bi-dash";
 
+    delBtn.appendChild(delIcon);
+
+    // Append elements
+
+    div.appendChild(colWidth);
     div.appendChild(line);
     div.appendChild(delBtn);
-    delBtn.appendChild(delIcon);
 
     let lines = document.getElementById("lines");
     lines.appendChild(div);
